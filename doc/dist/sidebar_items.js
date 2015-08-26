c294c0dd29dbd094dfc3185e543f254960ac4015
Jason Legler <jlegler@gmail.com>
2015-08-26 09:04:27 -0600

    Updated docs

---
+++ doc/dist/sidebar_items.js
@@ -0,0 +1,35 @@
+sidebarNodes = {
+  "modules": [
+    {"id": "TheFuzz",
+"docs": ["compare/3","compare/4"]
+},{"id": "TheFuzz.Phonetic.MetaphoneAlgorithm",
+"docs": ["compute/1"]
+},{"id": "TheFuzz.Phonetic.MetaphoneMetric",
+"docs": ["compare/2"]
+},{"id": "TheFuzz.Similarity.DiceSorensen",
+"docs": ["compare/2","compare/3"]
+},{"id": "TheFuzz.Similarity.Hamming",
+"docs": ["compare/2"]
+},{"id": "TheFuzz.Similarity.Jaccard",
+"docs": ["compare/2","compare/3"]
+},{"id": "TheFuzz.Similarity.Jaro",
+"docs": ["compare/2"]
+},{"id": "TheFuzz.Similarity.JaroWinkler",
+"docs": ["compare/2"]
+},{"id": "TheFuzz.Similarity.Levenshtein",
+"docs": ["compare/2"]
+},{"id": "TheFuzz.Similarity.NGram",
+"docs": ["compare/2","compare/3"]
+},{"id": "TheFuzz.Similarity.Overlap",
+"docs": ["compare/2","compare/3"]
+},{"id": "TheFuzz.Similarity.WeightedLevenshtein",
+"docs": ["compare/2","compare/3"]
+},{"id": "TheFuzz.StringMetric",
+"docs": ["compare/2"]
+},{"id": "TheFuzz.Util",
+"docs": ["deduplicate/1","intersect/2","is_alphabetic?/1","len/1","ngram_tokenize/2"]
+
+  }]
+
+};
+fillSidebarWithNodes(sidebarNodes);
